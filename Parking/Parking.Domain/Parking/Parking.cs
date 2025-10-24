49f5b4d87dbfd9601193bac468dc04e4ccf59377
ShadowZanko <vitassherbat@gmail.com>
2025-10-25 00:57:51 +0700

    Task4

--- Parking/Parking.Domain/Parking/Parking.cs
+++ Parking/Parking.Domain/Parking/Parking.cs
@@ -1,26 +1,27 @@
-﻿using Parking.Domain.Parking.Subscriptions;
+﻿using Domain.Parking;
+using SubscriptionType = Parking.Domain.Parking.Subscriptions.Subscriptions; // Псевдоним
 
 namespace Domain.Parking
 {
     public class Parking
     {
-        public Subscriptions Subscriptions { get; private set; }
-        public IDParking ParkingID { get; private set; }
-        public NumberOfSeats NumberOfSeats { get; private set; }
-        public Tariffs Tariffs { get; private set; }
-        public Customer Customer { get; private set; }
+        public SubscriptionType Subscription { get; }
+        public IDParking ParkingID { get; }
+        public NumberOfSeats NumberOfSeats { get; }
+        public Tariffs Tariff { get; }
+        public Customer Customer { get; }
 
         public Parking(
-            Subscriptions subscriptions,
+            SubscriptionType subscription,
             IDParking parkingID,
             NumberOfSeats numberOfSeats,
-            Tariffs tariffs,
+            Tariffs tariff,
             Customer customer)
         {
-            Subscriptions = subscriptions;
+            Subscription = subscription;
             ParkingID = parkingID;
             NumberOfSeats = numberOfSeats;
-            Tariffs = tariffs;
+            Tariff = tariff;
             Customer = customer;
         }
     }
